507bf4520254546308e2b6bd0adaccb659d8512c
bison-- <mike.bison42@googlemail.com>
2017-11-16 20:47:56 +0100

    aufgePEPt

--- simple.py
+++ simple.py
@@ -1,4 +1,3 @@
-import sys
 import time
 
 testChars = "abcdefghijklmnopqrstuvwxyz"
@@ -8,44 +7,43 @@ testChars = "abcdefghijklmnopqrstuvwxyz"
 def checkPwForLen(pwLen):
     global printCount
 
-    finalPw = [testChars[0]] * pwLen
-    while finalPw != [testChars[-1]] * pwLen:
-        incrementPw(finalPw)
+    generatedPw = [testChars[0]] * pwLen
+    while generatedPw != [testChars[-1]] * pwLen:
+        incrementPw(generatedPw)
 
         printCount += 1
         if printCount % 10000 == 0:
-            #pass
-            print('TESTING:', finalPw)
+            print('TESTING:', generatedPw)
 
-        if finalPw == pwList:
-            print('TESTING:', finalPw)
-            print('FOUND:', ''.join(finalPw))
+        if generatedPw == pwList:
+            print('TESTING:', generatedPw)
+            print('FOUND:', ''.join(generatedPw))
             return True
-
     return False
-def incrementPw(pw=[]):
+
+
+def incrementPw(generatedPw):
     incr = 0
     while True:
-        pos = testChars.find(pw[incr])
+        pos = testChars.find(generatedPw[incr])
         if pos == len(testChars)-1:
-            pw[incr] = testChars[0]
+            generatedPw[incr] = testChars[0]
             incr += 1
-            if incr > len(pw)-1:
+            if incr > len(generatedPw)-1:
                 break
         else:
-            pw[incr] = testChars[pos + 1]
+            generatedPw[incr] = testChars[pos + 1]
             break
 
-pw = input("Enter Password: ")
-pwList = list(pw)
+userPw = input("Enter Password: ")
+pwList = list(userPw)
 maxPwLen = len(pwList)
 printCount = 0
 
 start = time.time()
 for i in range(maxPwLen):
-   if checkPwForLen(i + 1):
-       break
-
+    if checkPwForLen(i + 1):
+        break
 stop = time.time()
 
 print('DURATION:', stop - start)
